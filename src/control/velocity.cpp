105f4b36dc06a6f736a0df6b7b41dead37c97efc
Ryan Benasutti <firey45@gmail.com>
2017-09-15 13:17:06 -0400

    ammend prev

---
+++ src/control/velocity.cpp
@@ -0,0 +1,16 @@
+#include "control/velocity.h"
+#include <API.h>
+
+namespace okapi {
+  float VelMath::loop(const float inewPos) {
+    const long now = millis();
+
+    vel = (now - lastTime) * (inewPos - lastPos) * 60.0 / ticksPerRev;
+    vel = filter.filter(vel);
+
+    lastPos = inewPos;
+    lastTime = now;
+
+    return vel;
+  }
+}
