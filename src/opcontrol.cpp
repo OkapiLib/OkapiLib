0bf98545fd32b04415f027c20046411c055c889d
Ryan Benasutti <firey45@gmail.com>
2017-10-02 15:47:53 -0400

    Added filter comparison test

--- src/opcontrol.cpp
+++ src/opcontrol.cpp
@@ -22,29 +22,6 @@
 
 using namespace okapi;
 
-float measureRPM(const unsigned char motorPort, const Encoder enc1, const Encoder enc2) {
-	using namespace std;
-
-	encoderReset(enc1);
-	encoderReset(enc2);
-
-	Timer timer;
-	timer.placeMark();
-
-	while (timer.getDtFromMark() < 5000) {
-		motorSet(motorPort, 127);
-		printf("%d,%d\n",encoderGet(enc1),encoderGet(enc2));
-		taskDelay(15);
-	}
-
-	motorSet(motorPort, 0);
-
-	if (abs(encoderGet(enc1)) > abs(encoderGet(enc2)))
-		return fabs(((float)encoderGet(enc1) / 360.0) * 12.0);
-
-	return fabs(((float)encoderGet(enc2) / 360.0) * 12.0);
-}
-
 void operatorControl() {
 	using namespace std; //Needed to get round to compile
 
@@ -59,20 +36,7 @@ void operatorControl() {
 	constexpr int liftUpTarget = 2570, lift34 = 300, liftDownTarget = 10;
 	int target = liftUpTarget;
 
-	// constexpr unsigned char motor1 = 2, motor2 = 3;
-	// lcdSetBacklight(uart1, true);
-
-	// int power = 0;
-  VelMath vm(360);
-
 	while (1) {
-	  // controller.driveForward(30);
-		// controller.turnClockwise(30);
-		// printf("%d,%d\n",encoderGet(leftEnc), encoderGet(rightEnc));
-
-		// volatile int x = *((int*)0xFFFFFFFF) = 69;
-    // printf("%d", x);
-
 		if (joystickGetDigital(1, 6, JOY_UP))
 			target = liftUpTarget;
 		else if (joystickGetDigital(1, 6, JOY_DOWN))
@@ -87,46 +51,14 @@ void operatorControl() {
     liftController.setTarget(target);
     liftController.loop(analogRead(liftPot));
 
-		controller.arcade(joystickGetAnalog(1, 3), joystickGetAnalog(1, 4));
-
-		// controller.driveForward(power);
-		if (joystickGetDigital(1, 8, JOY_RIGHT)) {
-			encoderReset(leftEnc);
-			encoderReset(rightEnc);
-			VelPid left(0.05, 0), right(0.05, 0);
-			left.setTarget(50);
-			right.setTarget(25);
-			while (true) {
-				left.loop(encoderGet(leftEnc));
-				right.loop(encoderGet(rightEnc));
-				controller.tank(left.getOutput(), right.getOutput());
-				// printf("%1.2f,%1.2f\n", left.getVel(), right.getVel());
-				if (joystickGetDigital(1, 7, JOY_RIGHT)) break;
-				taskDelay(15);
-			}
-		}
-
-		// vm.loop((encoderGet(leftEnc) + encoderGet(rightEnc)) / 2.0);
-		// printf("%d,%1.2f\n", power, vm.getOutput());
-		// power++;
-		// if (power > 127) break;
-		// if (joystickGetDigital(1, 8, JOY_LEFT)) break;
-		// taskDelay(50);
-
-		// while (lcdReadButtons(uart1) != LCD_BTN_CENTER)
-		// 	taskDelay(15);
-		//
-		// const float rpm1 = measureRPM(motor1, leftEnc, rightEnc);
-		// const float rpm2 = measureRPM(motor2, leftEnc, rightEnc);
-		//
-		// char line1[16], line2[16];
-		// line1[15] = '\0';
-		// line2[15] = '\0';
-		// sprintf(line1, "1:%d,2:%d", (int)round(rpm1), (int)round(rpm2));
-		// sprintf(line2, "Voltage: %1.2f", powerLevelMain() / 1000.0);
-		// lcdSetText(uart1, 1, line1);
-		// lcdSetText(uart1, 2, line2);
-
-		// taskDelay(15);
+    controller.arcade(joystickGetAnalog(1, 3), joystickGetAnalog(1, 4));
+    
+    if (joystickGetDigital(1, 8, JOY_RIGHT)) {
+      VelMath vm1(360), vm2(360, 1, 0);
+      float avg = (encoderGet(leftEnc) + encoderGet(rightEnc)) / 2.0;
+      vm1.loop(avg);
+      vm2.loop(avg);
+      printf("%1.2f,%1.2f\n", vm1.getOutput(), vm2.getOutput());
+    }
 	}
 }
