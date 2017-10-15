c39f0a878dc1d97a0454c0c4cd08774fca6170e4
Ryan Benasutti <firey45@gmail.com>
2017-10-14 22:49:22 -0400

    Added potentiometer

--- include/device/button.h
+++ include/device/button.h
@@ -1,14 +1,20 @@
 #ifndef OKAPI_BUTTON
 #define OKAPI_BUTTON
 
+#include <API.h>
+
 namespace okapi {
   class Button {
   public:
     explicit constexpr Button():
       port(0),
       inverted(false) {}
+      
+    explicit constexpr Button(const unsigned long long int iport):
+      port(iport),
+      inverted(false) {}
 
-    explicit constexpr Button()(const unsigned long long int iport, const bool iinverted):
+    explicit constexpr Button(const unsigned long long int iport, const bool iinverted):
       port(iport),
       inverted(iinverted) {}
 
@@ -18,8 +24,8 @@ namespace okapi {
     const bool inverted;
   };
 
-  constexpr Button operator"" _b(const unsigned long long int b) { return Button(static_cast<unsigned char>(b), false); }
-  constexpr Button operator"" _ib(const unsigned long long int b) { return Button(static_cast<unsigned char>(b), true); }
+  constexpr Button operator"" _b(const unsigned long long int p) { return Button(static_cast<unsigned char>(p), false); }
+  constexpr Button operator"" _ib(const unsigned long long int p) { return Button(static_cast<unsigned char>(p), true); }
 }
 
 #endif /* end of include guard: OKAPI_BUTTON */
---
+++ include/device/potentiometer.h
@@ -0,0 +1,31 @@
+#ifndef OKAPI_POTENTIOMETER
+#define OKAPI_POTENTIOMETER
+
+#include <API.h>
+
+namespace okapi {
+  class Potentiometer {
+  public:
+    explicit constexpr Potentiometer():
+      port(0),
+      inverted(false) {}
+      
+    explicit constexpr Potentiometer(const unsigned long long int iport):
+      port(iport),
+      inverted(false) {}
+
+    explicit constexpr Potentiometer(const unsigned long long int iport, const bool iinverted):
+      port(iport),
+      inverted(iinverted) {}
+
+    int get() const { return inverted ? 4095 - analogRead(port) : analogRead(port); }
+  private:
+    const unsigned char port;
+    const bool inverted;
+  };
+
+  constexpr Potentiometer operator"" _p(const unsigned long long int p) { return Potentiometer(static_cast<unsigned char>(p), false); }
+  constexpr Potentiometer operator"" _ip(const unsigned long long int p) { return Potentiometer(static_cast<unsigned char>(p), true); }
+}
+
+#endif /* end of include guard: OKAPI_POTENTIOMETER */
