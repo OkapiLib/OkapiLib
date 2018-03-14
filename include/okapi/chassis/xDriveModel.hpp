352c67f427fb49216c637308d34fedaf3ea27f95
Ryan <firey45@gmail.com>
2018-03-13 22:07:18 -0400

    Split XDriveModel implementation

--- include/okapi/chassis/xDriveModel.hpp
+++ include/okapi/chassis/xDriveModel.hpp
@@ -9,32 +9,20 @@
 #include "okapi/device/rotarySensor.hpp"
 
 namespace okapi {
-class XDriveModel;
-
 class XDriveModelParams : public ChassisModelParams {
   public:
   XDriveModelParams(const AbstractMotor &itopLeftMotor, const AbstractMotor &itopRightMotor,
                     const AbstractMotor &ibottomRightMotor, const AbstractMotor &ibottomLeftMotor,
-                    const RotarySensor &ileftEnc, const RotarySensor &irightEnc)
-    : topLeftMotor(itopLeftMotor),
-      topRightMotor(itopRightMotor),
-      bottomRightMotor(ibottomRightMotor),
-      bottomLeftMotor(ibottomLeftMotor),
-      leftSensor(ileftEnc),
-      rightSensor(irightEnc) {
-  }
+                    const RotarySensor &ileftEnc, const RotarySensor &irightEnc);
 
-  virtual ~XDriveModelParams() {
-  }
+  virtual ~XDriveModelParams();
 
   /**
    * Consutructs a new XDriveModel.
    *
    * @return const reference to the ChassisModel
    */
-  const ChassisModel &make() const override {
-    return XDriveModel(*this);
-  }
+  const ChassisModel &make() const override;
 
   const AbstractMotor &topLeftMotor;
   const AbstractMotor &topRightMotor;
@@ -59,126 +47,36 @@ class XDriveModel : public ChassisModel {
    */
   XDriveModel(const AbstractMotor &itopLeftMotor, const AbstractMotor &itopRightMotor,
               const AbstractMotor &ibottomRightMotor, const AbstractMotor &ibottomLeftMotor,
-              const RotarySensor &ileftEnc, const RotarySensor &irightEnc)
-    : topLeftMotor(itopLeftMotor),
-      topRightMotor(itopRightMotor),
-      bottomRightMotor(ibottomRightMotor),
-      bottomLeftMotor(ibottomLeftMotor),
-      leftSensor(ileftEnc),
-      rightSensor(irightEnc) {
-  }
+              const RotarySensor &ileftEnc, const RotarySensor &irightEnc);
 
-  XDriveModel(const XDriveModelParams &iparams)
-    : topLeftMotor(iparams.topLeftMotor),
-      topRightMotor(iparams.topRightMotor),
-      bottomRightMotor(iparams.bottomRightMotor),
-      bottomLeftMotor(iparams.bottomLeftMotor),
-      leftSensor(iparams.leftSensor),
-      rightSensor(iparams.rightSensor) {
-  }
+  XDriveModel(const XDriveModelParams &iparams);
 
-  XDriveModel(const XDriveModel &other)
-    : topLeftMotor(other.topLeftMotor),
-      topRightMotor(other.topRightMotor),
-      bottomRightMotor(other.bottomRightMotor),
-      bottomLeftMotor(other.bottomLeftMotor),
-      leftSensor(other.leftSensor),
-      rightSensor(other.rightSensor) {
-  }
+  XDriveModel(const XDriveModel &other);
 
-  virtual ~XDriveModel() = default;
+  virtual ~XDriveModel();
 
-  void driveForward(const int ipower) const override {
-    topLeftMotor.set_velocity(ipower);
-    topRightMotor.set_velocity(ipower);
-    bottomRightMotor.set_velocity(ipower);
-    bottomLeftMotor.set_velocity(ipower);
-  }
+  void driveForward(const int ipower) const override;
 
-  void driveVector(const int idistPower, const int ianglePower) const override {
-    topLeftMotor.set_velocity(idistPower + ianglePower);
-    topRightMotor.set_velocity(idistPower - ianglePower);
-    bottomRightMotor.set_velocity(idistPower - ianglePower);
-    bottomLeftMotor.set_velocity(idistPower + ianglePower);
-  }
+  void driveVector(const int idistPower, const int ianglePower) const override;
 
-  void turnClockwise(const int ipower) const override {
-    topLeftMotor.set_velocity(ipower);
-    topRightMotor.set_velocity(-1 * ipower);
-    bottomRightMotor.set_velocity(-1 * ipower);
-    bottomLeftMotor.set_velocity(ipower);
-  }
+  void turnClockwise(const int ipower) const override;
 
-  void stop() const override {
-    topLeftMotor.set_velocity(0);
-    topRightMotor.set_velocity(0);
-    bottomRightMotor.set_velocity(0);
-    bottomLeftMotor.set_velocity(0);
-  }
+  void stop() const override;
 
-  void tank(const int ileftVal, const int irightVal, const int ithreshold = 0) const override {
-    if (fabs(ileftVal) < ithreshold) {
-      topLeftMotor.set_velocity(0);
-      bottomLeftMotor.set_velocity(0);
-    } else {
-      topLeftMotor.set_velocity(ileftVal);
-      bottomLeftMotor.set_velocity(ileftVal);
-    }
+  void tank(const int ileftVal, const int irightVal, const int ithreshold = 0) const override;
 
-    if (fabs(irightVal) < ithreshold) {
-      topRightMotor.set_velocity(0);
-      bottomRightMotor.set_velocity(0);
-    } else {
-      topRightMotor.set_velocity(irightVal);
-      bottomRightMotor.set_velocity(irightVal);
-    }
-  }
-
-  void arcade(int iverticalVal, int ihorizontalVal, const int ithreshold = 0) const override {
-    if (fabs(iverticalVal) < ithreshold)
-      iverticalVal = 0;
-    if (fabs(ihorizontalVal) < ithreshold)
-      ihorizontalVal = 0;
-
-    topLeftMotor.set_velocity(iverticalVal + ihorizontalVal);
-    topRightMotor.set_velocity(iverticalVal - ihorizontalVal);
-    bottomRightMotor.set_velocity(iverticalVal - ihorizontalVal);
-    bottomLeftMotor.set_velocity(iverticalVal + ihorizontalVal);
-  }
+  void arcade(int iverticalVal, int ihorizontalVal, const int ithreshold = 0) const override;
 
   void xArcade(int iverticalVal, int ihorizontalVal, int irotateVal,
-               const int ithreshold = 0) const {
-    if (fabs(iverticalVal) < ithreshold)
-      iverticalVal = 0;
-    if (fabs(ihorizontalVal) < ithreshold)
-      ihorizontalVal = 0;
-    if (fabs(irotateVal) < ithreshold)
-      irotateVal = 0;
+               const int ithreshold = 0) const;
 
-    topLeftMotor.set_velocity(iverticalVal + ihorizontalVal + irotateVal);
-    topRightMotor.set_velocity(iverticalVal - ihorizontalVal - irotateVal);
-    bottomRightMotor.set_velocity(iverticalVal + ihorizontalVal - irotateVal);
-    bottomLeftMotor.set_velocity(iverticalVal - ihorizontalVal + irotateVal);
-  }
+  void left(const int ipower) const override;
 
-  void left(const int ipower) const override {
-    topLeftMotor.set_velocity(ipower);
-    bottomLeftMotor.set_velocity(ipower);
-  }
+  void right(const int ipower) const override;
 
-  void right(const int ipower) const override {
-    topRightMotor.set_velocity(ipower);
-    bottomRightMotor.set_velocity(ipower);
-  }
+  std::valarray<int> getSensorVals() const override;
 
-  std::valarray<int> getSensorVals() const override {
-    return std::valarray<int>{leftSensor.get(), rightSensor.get()};
-  }
-
-  void resetSensors() const override {
-    leftSensor.reset();
-    rightSensor.reset();
-  }
+  void resetSensors() const override;
 
   private:
   const AbstractMotor &topLeftMotor;
