19b3d7ff4d378a899d891f56c62671f2545261f3
Ryan Benasutti <firey45@gmail.com>
2018-03-16 17:25:46 -0400

    Add AsyncController

---
+++ include/okapi/control/asyncPositionController.hpp
@@ -0,0 +1,13 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+#ifndef _OKAPI_ASYNCPOSITIONCONTROLLER_HPP_
+#define _OKAPI_ASYNCPOSITIONCONTROLLER_HPP_
+
+#include "okapi/control/asyncController.hpp"
+
+namespace okapi {
+class AsyncPositionController : public AsyncController {};
+} // namespace okapi
+
+#endif
