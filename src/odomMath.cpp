4a6eabb1f91bd7cce353754c656820bdaf80e5d5
Ryan Benasutti <firey45@gmail.com>
2018-03-17 21:52:46 -0400

    clang-format

--- src/odomMath.cpp
+++ src/odomMath.cpp
@@ -1,9 +1,9 @@
 /* This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
-#include <cmath>
 #include "okapi/odometry/odomMath.hpp"
 #include "okapi/util/mathUtil.hpp"
+#include <cmath>
 
 namespace okapi {
 DistanceAndAngle::DistanceAndAngle(const double ilength, const double itheta)
@@ -59,9 +59,9 @@ DistanceAndAngle OdomMath::computeDistanceAndAngleToPoint(const double ix, const
 }
 
 std::tuple<double, double> OdomMath::guessScales(const double chassisDiam, const double wheelDiam,
-                                               const double ticksPerRev) {
+                                                 const double ticksPerRev) {
   const double scale = ((wheelDiam * pi * inchToMM) / ticksPerRev) *
-                      0.9945483364; // The scale is usually off by this amount
+                       0.9945483364; // The scale is usually off by this amount
   const double turnScale = (1.0 / (chassisDiam * inchToMM)) * radianToDegree * 2;
   return std::make_tuple(scale, turnScale);
 }
