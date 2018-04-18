aa4bef2411221e5724cb5232b00b1b5a83f0890a
Ryan Benasutti <firey45@gmail.com>
2018-04-18 14:17:21 -0400

    Add FakeIt submodule and simple test

--- src/opcontrol.cpp
+++ src/opcontrol.cpp
@@ -1,5 +1,6 @@
 #include "api.h"
 
+#include "okapi/FakeIt/single_header/standalone/fakeit.hpp"
 #include "okapi/api.hpp"
 #include "okapi/test/testRunner.hpp"
 
@@ -273,6 +274,32 @@ void runHeadlessUnitTests() {
   }
 
   test_print_report();
+}
+
+struct SomeInterface {
+  virtual int foo(int) = 0;
+  virtual int bar(int, int) = 0;
+};
+
+void testFakeIt() {
+  using namespace fakeit;
+  Mock<SomeInterface> mock;
+  // Stub a method to return a value once
+  When(Method(mock, foo)).Return(1);
+
+  // Stub multiple return values (The next two lines do exactly the same)
+  When(Method(mock, foo)).Return(1, 2, 3);
+  When(Method(mock, foo)).Return(1).Return(2).Return(3);
+
+  // Return the same value many times (56 in this example)
+  When(Method(mock, foo)).Return(56_Times(1));
+
+  // Return many values many times (First 100 calls will return 1, next 200 calls will return 2)
+  When(Method(mock, foo)).Return(100_Times(1), 200_Times(2));
+
+  // Always return a value (The next two lines do exactly the same)
+  When(Method(mock, foo)).AlwaysReturn(1);
+  Method(mock, foo) = 1;
 }
 
 void constructorTests() {
