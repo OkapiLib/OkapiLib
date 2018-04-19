a68404c5e92a98c6b0a375e7f254c6a0debfae71
Ryan Benasutti <firey45@gmail.com>
2018-04-19 12:17:24 -0400

    Add AsyncVelIntegratedController tests

--- src/opcontrol.cpp
+++ src/opcontrol.cpp
@@ -141,8 +141,8 @@ void testAsyncControllers() {
         return 1;
       }
 
-      virtual std::int32_t moveVelocity(const std::int16_t ivelocity) const override {
-        lastVelocity = ivelocity;
+      virtual std::int32_t moveVoltage(const std::int16_t ivoltage) const override {
+        lastVoltage = ivoltage;
         return 1;
       }
 
@@ -150,7 +150,7 @@ void testAsyncControllers() {
         return 0;
       }
 
-      mutable std::int16_t lastVelocity;
+      mutable std::int16_t lastVoltage;
       mutable std::int16_t lastPosition;
     };
 
@@ -163,7 +163,7 @@ void testAsyncControllers() {
 
     controller.flipDisable();
     test("Disabling the controller should turn the motor off",
-         TEST_BODY(AssertThat, motor->lastVelocity, Equals(0)));
+         TEST_BODY(AssertThat, motor->lastVoltage, Equals(0)));
 
     controller.flipDisable();
     test("Re-enabling the controller should move the motor to the previous target",
@@ -172,10 +172,60 @@ void testAsyncControllers() {
     controller.flipDisable();
     controller.reset();
     test("Resetting the controller should not change the current target",
-         TEST_BODY(AssertThat, motor->lastVelocity, Equals(0)));
+         TEST_BODY(AssertThat, motor->lastVoltage, Equals(0)));
     controller.flipDisable();
     test("Re-enabling the controller after a reset should not move the motor",
          TEST_BODY(AssertThat, motor->lastPosition, Equals(0)));
+  }
+
+  {
+    test_printf("Testing AsyncVelIntegratedController");
+
+    class MockMotor : public Motor {
+      public:
+      MockMotor() : Motor(1) {
+      }
+
+      virtual std::int32_t moveVelocity(const std::int16_t ivelocity) const override {
+        lastVelocity = ivelocity;
+        return 1;
+      }
+
+      virtual std::int32_t moveVoltage(const std::int16_t ivoltage) const override {
+        lastVoltage = ivoltage;
+        return 1;
+      }
+
+      virtual double getActualVelocity() const override {
+        return 0;
+      }
+
+      mutable std::int16_t lastVoltage;
+      mutable std::int16_t lastVelocity;
+    };
+
+    auto motor = std::make_shared<MockMotor>();
+
+    AsyncVelIntegratedController controller(motor);
+
+    controller.setTarget(100);
+    test("Should be on by default", TEST_BODY(AssertThat, motor->lastVelocity, Equals(100)));
+
+    controller.flipDisable();
+    test("Disabling the controller should turn the motor off",
+         TEST_BODY(AssertThat, motor->lastVoltage, Equals(0)));
+
+    controller.flipDisable();
+    test("Re-enabling the controller should move the motor to the previous target",
+         TEST_BODY(AssertThat, motor->lastVelocity, Equals(100)));
+
+    controller.flipDisable();
+    controller.reset();
+    test("Resetting the controller should not change the current target",
+         TEST_BODY(AssertThat, motor->lastVoltage, Equals(0)));
+    controller.flipDisable();
+    test("Re-enabling the controller after a reset should not move the motor",
+         TEST_BODY(AssertThat, motor->lastVelocity, Equals(0)));
   }
 }
 
