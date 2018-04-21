de6862a6a1678f722941e95ec9089a3153f9f4b3
Ryan Benasutti <firey45@gmail.com>
2018-04-21 11:35:56 -0400

    Issue #51: Rewrite AsyncPosPIDController in terms of AsyncWrapper

--- include/okapi/control/async/asyncPosPidController.hpp
+++ include/okapi/control/async/asyncPosPidController.hpp
@@ -10,6 +10,7 @@
 
 #include "api.h"
 #include "okapi/control/async/asyncPositionController.hpp"
+#include "okapi/control/async/asyncWrapper.hpp"
 #include "okapi/control/controllerInput.hpp"
 #include "okapi/control/controllerOutput.hpp"
 #include "okapi/control/iterative/iterativePosPidController.hpp"
@@ -30,7 +31,7 @@ class AsyncPosPIDControllerArgs : public AsyncPositionControllerArgs {
   const IterativePosPIDControllerArgs &params;
 };
 
-class AsyncPosPIDController : public AsyncPositionController {
+class AsyncPosPIDController : public AsyncWrapper {
   public:
   AsyncPosPIDController(std::shared_ptr<ControllerInput> iinput,
                         std::shared_ptr<ControllerOutput> ioutput,
@@ -39,80 +40,6 @@ class AsyncPosPIDController : public AsyncPositionController {
   AsyncPosPIDController(std::shared_ptr<ControllerInput> iinput,
                         std::shared_ptr<ControllerOutput> ioutput, const double ikP,
                         const double ikI, const double ikD, const double ikBias = 0);
-
-  /**
-   * Sets the target for the controller.
-   */
-  virtual void setTarget(const double itarget) override;
-
-  /**
-   * Returns the last calculated output of the controller. Default is 0.
-   */
-  virtual double getOutput() const override;
-
-  /**
-   * Returns the last error of the controller.
-   */
-  virtual double getError() const override;
-
-  /**
-   * Returns whether the controller has settled at the target. Determining what settling means is
-   * implementation-dependent.
-   *
-   * @return whether the controller is settled
-   */
-  virtual bool isSettled() override;
-
-  /**
-   * Set time between loops in ms. Default does nothing.
-   *
-   * @param isampleTime time between loops in ms
-   */
-  virtual void setSampleTime(const std::uint32_t isampleTime) override;
-
-  /**
-   * Set controller output bounds. Default does nothing.
-   *
-   * @param imax max output
-   * @param imin min output
-   */
-  virtual void setOutputLimits(double imax, double imin) override;
-
-  /**
-   * Resets the controller so it can start from 0 again properly. Keeps configuration from
-   * before.
-   */
-  virtual void reset() override;
-
-  /**
-   * Changes whether the controller is off or on. Turning the controller on after it was off will
-   * cause the controller to move to its last set target, unless it was reset in that time.
-   */
-  virtual void flipDisable() override;
-
-  /**
-   * Sets whether the controller is off or on. Turning the controller on after it was off will
-   * cause the controller to move to its last set target, unless it was reset in that time.
-   *
-   * @param iisDisabled whether the controller is disabled
-   */
-  virtual void flipDisable(const bool iisDisabled) override;
-
-  /**
-   * Returns whether the controller is currently disabled.
-   *
-   * @return whether the controller is currently disabled
-   */
-  virtual bool isDisabled() const override;
-
-  protected:
-  std::shared_ptr<ControllerInput> input;
-  std::shared_ptr<ControllerOutput> output;
-  IterativePosPIDController controller;
-  pros::Task task;
-
-  static void trampoline(void *context);
-  void step();
 };
 } // namespace okapi
 
--- src/control/async/asyncPosPidController.cpp
+++ src/control/async/asyncPosPidController.cpp
@@ -17,68 +17,14 @@ AsyncPosPIDControllerArgs::AsyncPosPIDControllerArgs(std::shared_ptr<ControllerI
 AsyncPosPIDController::AsyncPosPIDController(std::shared_ptr<ControllerInput> iinput,
                                              std::shared_ptr<ControllerOutput> ioutput,
                                              const IterativePosPIDControllerArgs &iparams)
-  : input(iinput), output(ioutput), controller(iparams), task(trampoline, this) {
+  : AsyncWrapper(iinput, ioutput, std::make_unique<IterativePosPIDController>(iparams)) {
 }
 
 AsyncPosPIDController::AsyncPosPIDController(std::shared_ptr<ControllerInput> iinput,
                                              std::shared_ptr<ControllerOutput> ioutput,
                                              const double ikP, const double ikI, const double ikD,
                                              const double ikBias)
-  : input(iinput), output(ioutput), controller(ikP, ikI, ikD, ikBias), task(trampoline, this) {
-}
-
-void AsyncPosPIDController::step() {
-  std::uint32_t prevTime = 0;
-
-  while (true) {
-    if (!controller.isDisabled()) {
-      output->controllerSet(controller.step(input->controllerGet()));
-    }
-    task.delay_until(&prevTime, controller.getSampleTime());
-  }
-}
-
-void AsyncPosPIDController::trampoline(void *context) {
-  static_cast<AsyncPosPIDController *>(context)->step();
-}
-
-void AsyncPosPIDController::setTarget(const double itarget) {
-  controller.setTarget(itarget);
-}
-
-double AsyncPosPIDController::getOutput() const {
-  return controller.getOutput();
-}
-
-double AsyncPosPIDController::getError() const {
-  return controller.getError();
-}
-
-bool AsyncPosPIDController::isSettled() {
-  return controller.isSettled();
-}
-
-void AsyncPosPIDController::setSampleTime(const std::uint32_t isampleTime) {
-  controller.setSampleTime(isampleTime);
-}
-
-void AsyncPosPIDController::setOutputLimits(double imax, double imin) {
-  controller.setOutputLimits(imax, imin);
-}
-
-void AsyncPosPIDController::reset() {
-  controller.reset();
-}
-
-void AsyncPosPIDController::flipDisable() {
-  controller.flipDisable();
-}
-
-void AsyncPosPIDController::flipDisable(const bool iisDisabled) {
-  controller.flipDisable(iisDisabled);
-}
-
-bool AsyncPosPIDController::isDisabled() const {
-  return controller.isDisabled();
+  : AsyncWrapper(iinput, ioutput,
+                 std::make_unique<IterativePosPIDController>(ikP, ikI, ikD, ikBias)) {
 }
 } // namespace okapi
