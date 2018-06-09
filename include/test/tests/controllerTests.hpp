1b7ebf95785607c1cebf3d009394a206ecafff0b
Ryan Benasutti <firey45@gmail.com>
2018-06-09 16:54:03 -0400

    Issue #1: Add test for feed-forward

--- include/test/tests/controllerTests.hpp
+++ include/test/tests/controllerTests.hpp
@@ -83,6 +83,21 @@ void testIterativeControllers() {
     test("IterativeVelPIDController should settle after 2000 iterations (controller error is "
          "correct)",
          TEST_BODY(AssertThat, controller.getError(), EqualsWithDelta(0, 0.01)));
+
+    IterativeVelPIDController controller2(
+      0, 0, 0.1,
+      std::make_unique<VelMath>(1800, std::make_shared<PassthroughFilter>(),
+                                std::make_unique<MockTimer>()),
+      std::make_unique<MockTimer>(), std::make_unique<SettledUtil>());
+
+    controller2.setTarget(5);
+    for (size_t i = 0; i < 5; i++) {
+      test(
+        "IterativeVelPIDController with feed-forward should output proportional to error and not "
+        "integrate " +
+          std::to_string(i),
+        TEST_BODY(AssertThat, controller2.step(0), EqualsWithDelta(0.5, 0.01)));
+    }
   }
 
   {
