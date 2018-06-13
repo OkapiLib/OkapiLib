9572a8d78e42c29fe5ee33b13ffe47ee2c2615ec
Ryan Benasutti <firey45@gmail.com>
2018-06-12 22:17:13 -0400

    Issue #75: Add AbstractTimer and expose millis

--- include/okapi/api/util/abstractTimer.hpp
+++ include/okapi/api/util/abstractTimer.hpp
@@ -5,75 +5,77 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/.
  */
-#ifndef _OKAPI_TIMER_HPP_
-#define _OKAPI_TIMER_HPP_
+#ifndef _OKAPI_ABSTRACTTIMER_HPP_
+#define _OKAPI_ABSTRACTTIMER_HPP_
 
-#include "okapi/api/util/abstractTimer.hpp"
+#include "api.h"
+#include "okapi/api/units/QFrequency.hpp"
+#include "okapi/api/units/QTime.hpp"
 
 namespace okapi {
-class Timer : public AbstractTimer {
+class AbstractTimer {
   public:
-  Timer();
+  virtual ~AbstractTimer();
 
   /**
    * Returns the current time in units of QTime.
    *
    * @return the current time
    */
-  virtual QTime millis() const override;
+  virtual QTime millis() const = 0;
 
   /**
    * Returns the time passed in ms since the previous call of this function.
    *
    * @return The time passed in ms since the previous call of this function
    */
-  virtual QTime getDt() override;
+  virtual QTime getDt() = 0;
 
   /**
    * Returns the time the timer was first constructed.
    *
    * @return The time the timer was first constructed
    */
-  virtual QTime getStartingTime() const override;
+  virtual QTime getStartingTime() const = 0;
 
   /**
    * Returns the time since the timer was first constructed.
    *
    * @return The time since the timer was first constructed
    */
-  virtual QTime getDtFromStart() const override;
+  virtual QTime getDtFromStart() const = 0;
 
   /**
    * Place a time marker. Placing another marker will overwrite the previous one.
    */
-  virtual void placeMark() override;
+  virtual void placeMark() = 0;
 
   /**
    * Place a hard time marker. Placing another hard marker will not overwrite the previous one;
    * instead, call clearHardMark() and then place another.
    */
-  virtual void placeHardMark() override;
+  virtual void placeHardMark() = 0;
 
   /**
    * Clears the hard marker.
    *
    * @return The old hard marker
    */
-  virtual QTime clearHardMark() override;
+  virtual QTime clearHardMark() = 0;
 
   /**
    * Returns the time since the time marker.
    *
    * @return The time since the time marker
    */
-  virtual QTime getDtFromMark() const override;
+  virtual QTime getDtFromMark() const = 0;
 
   /**
    * Returns the time since the hard time marker.
    *
    * @return The time since the hard time marker
    */
-  virtual QTime getDtFromHardMark() const override;
+  virtual QTime getDtFromHardMark() const = 0;
 
   /**
    * Returns true when the input time period has passed, then resets. Meant to be used in loops
@@ -83,7 +85,7 @@ class Timer : public AbstractTimer {
    * @return true when the input time period has passed, false after reading true until the
    *   period has passed again
    */
-  virtual bool repeat(const QTime time) override;
+  virtual bool repeat(const QTime time) = 0;
 
   /**
    * Returns true when the input time period has passed, then resets. Meant to be used in loops
@@ -93,14 +95,7 @@ class Timer : public AbstractTimer {
    * @return true when the input time period has passed, false after reading true until the
    *   period has passed again
    */
-  virtual bool repeat(const QFrequency frequency) override;
-
-  protected:
-  QTime firstCalled;
-  QTime lastCalled;
-  QTime mark;
-  QTime hardMark;
-  QTime repeatMark;
+  virtual bool repeat(const QFrequency frequency) = 0;
 };
 } // namespace okapi
 
