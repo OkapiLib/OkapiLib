4aa22f6a6ce64253dd0cf69ecea968dd38029668
Ryan Benasutti <firey45@gmail.com>
2018-07-08 00:35:20 -0400

    Issue #90: Try using env var instead of uname

--- travisScript.sh
+++ travisScript.sh
@@ -1,16 +1,12 @@
 #!/bin/sh
 
-if `uname` = "Darwin"; then
+if "x$ARCH" = "xlinux"; then
     mkdir cmake-build-debug
-    cd cmake-build-debug
-    cmake -DCMAKE_BUILD_TYPE=Debug -G "CodeBlocks - Unix Makefiles" ..
-    cmake --build . --target OkapiLibV5 -- -j 2
-    ./OkapiLibV5
-else
-    make
-    mkdir cmake-build-debug
-    cd cmake-build-debug
-    cmake -DCMAKE_BUILD_TYPE=Debug -G "CodeBlocks - Unix Makefiles" ..
-    cmake --build . --target OkapiLibV5 -- -j 2
-    ./OkapiLibV5
 fi
+
+make
+mkdir cmake-build-debug
+cd cmake-build-debug
+cmake -DCMAKE_BUILD_TYPE=Debug -G "CodeBlocks - Unix Makefiles" ..
+cmake --build . --target OkapiLibV5 -- -j 2
+./OkapiLibV5
