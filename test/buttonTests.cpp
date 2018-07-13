5c8bb006ec065b685017d62c3fbc36fcc629478a
Ryan Benasutti <firey45@gmail.com>
2018-07-12 22:57:05 -0400

    Add sanity check unit tests for QTime
    Also remove useless header files that aren't needed under gtest

--- test/buttonTests.cpp
+++ test/buttonTests.cpp
@@ -5,7 +5,6 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/.
  */
-#include "test/tests/api/buttonTests.hpp"
 #include "okapi/api/device/button/buttonBase.hpp"
 #include "test/crossPlatformTestRunner.hpp"
 #include <gtest/gtest.h>
--- test/chassisControllerTests.cpp
+++ test/chassisControllerTests.cpp
@@ -5,7 +5,6 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/.
  */
-#include "test/tests/api/chassisControllerTests.hpp"
 #include "okapi/api/chassis/controller/chassisScales.hpp"
 #include "test/crossPlatformTestRunner.hpp"
 #include <gtest/gtest.h>
