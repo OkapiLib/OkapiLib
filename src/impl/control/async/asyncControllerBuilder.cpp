6e80446b499da08a95277393aaa49e8b8092f59a
Ryan Benasutti <firey45@gmail.com>
2018-08-06 20:17:29 -0400

    Issue #79: Move AsyncControllerBuilder to impl

--- src/impl/control/async/asyncControllerBuilder.cpp
+++ src/impl/control/async/asyncControllerBuilder.cpp
@@ -5,7 +5,7 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/.
  */
-#include "okapi/api/control/async/asyncControllerBuilder.hpp"
+#include "okapi/impl/control/async/asyncControllerBuilder.hpp"
 #include "okapi/api/control/async/asyncWrapper.hpp"
 #include "okapi/api/control/iterative/iterativePosPidController.hpp"
 #include "okapi/api/control/iterative/iterativeVelPidController.hpp"
