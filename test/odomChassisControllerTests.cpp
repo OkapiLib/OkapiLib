1d598f1e92fba2a56994688c6257fd00516cac09
Ryan Benasutti <firey45@gmail.com>
2018-08-11 17:39:22 -0400

    Issue #38: Add copy of tests for CCPID

--- test/odomChassisControllerTests.cpp
+++ test/odomChassisControllerTests.cpp
@@ -5,8 +5,8 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/.
  */
-#include "okapi/api/chassis/controller/chassisControllerIntegrated.hpp"
 #include "okapi/api/chassis/controller/odomChassisControllerIntegrated.hpp"
+#include "okapi/api/chassis/controller/odomChassisControllerPid.hpp"
 #include "test/tests/api/implMocks.hpp"
 #include <gtest/gtest.h>
 
@@ -63,24 +63,98 @@ class OdomChassisControllerIntegratedTest : public ::testing::Test {
 TEST_F(OdomChassisControllerIntegratedTest, MoveBelowThreshold) {
   assertMotorsHaveBeenStopped(leftMotor, rightMotor);
 
-  drive->setMoveThreshold(10);
-  drive->driveToPoint(9, 0);
+  drive->setMoveThreshold(5);
+  drive->driveToPoint(4, 0);
 
-  EXPECT_DOUBLE_EQ(leftController->getError(), 0);
-  EXPECT_DOUBLE_EQ(rightController->getError(), 0);
+  EXPECT_DOUBLE_EQ(leftController->getTarget(), 0);
+  EXPECT_DOUBLE_EQ(rightController->getTarget(), 0);
 }
 
 TEST_F(OdomChassisControllerIntegratedTest, MoveAboveThreshold) {
   assertMotorsHaveBeenStopped(leftMotor, rightMotor);
 
-  drive->setMoveThreshold(10);
-  drive->driveToPoint(11, 0);
+  drive->setMoveThreshold(5);
+  drive->driveToPoint(6, 0);
 
-  EXPECT_DOUBLE_EQ(leftController->getError(), 11);
-  EXPECT_DOUBLE_EQ(rightController->getError(), 11);
+  EXPECT_DOUBLE_EQ(leftController->getTarget(), 6);
+  EXPECT_DOUBLE_EQ(rightController->getTarget(), 6);
 }
 
 TEST_F(OdomChassisControllerIntegratedTest, SetStateTest) {
+  auto stateBefore = drive->getState();
+  assertOdomStateEquals(0, 0, 0, stateBefore);
+
+  OdomState newState(1, 2, 3);
+  drive->setState(newState);
+
+  auto stateAfter = drive->getState();
+  assertOdomStateEquals(stateAfter, newState);
+}
+
+class OdomChassisControllerPIDTest : public ::testing::Test {
+  protected:
+  void SetUp() override {
+    scales = new ChassisScales({2, 2});
+    leftMotor = new MockMotor();
+    rightMotor = new MockMotor();
+
+    distanceController = new MockIterativeController();
+    angleController = new MockIterativeController();
+    turnController = new MockIterativeController();
+
+    model = new SkidSteerModel(std::unique_ptr<AbstractMotor>(leftMotor),
+                               std::unique_ptr<AbstractMotor>(rightMotor));
+
+    std::shared_ptr<SkidSteerModel> modelPtr = std::shared_ptr<SkidSteerModel>(model);
+
+    odom = new Odometry(modelPtr, *scales, createTimeUtil().getRate());
+
+    drive =
+      new OdomChassisControllerPID(createTimeUtil(), modelPtr, std::unique_ptr<Odometry>(odom),
+                                   std::unique_ptr<IterativePosPIDController>(distanceController),
+                                   std::unique_ptr<IterativePosPIDController>(angleController),
+                                   std::unique_ptr<IterativePosPIDController>(turnController));
+  }
+
+  void TearDown() override {
+    delete scales;
+    delete drive;
+  }
+
+  ChassisScales *scales;
+  MockMotor *leftMotor;
+  MockMotor *rightMotor;
+  MockIterativeController *distanceController;
+  MockIterativeController *angleController;
+  MockIterativeController *turnController;
+  SkidSteerModel *model;
+  Odometry *odom;
+  OdomChassisControllerPID *drive;
+};
+
+TEST_F(OdomChassisControllerPIDTest, MoveBelowThreshold) {
+  assertMotorsHaveBeenStopped(leftMotor, rightMotor);
+
+  drive->setMoveThreshold(5);
+  drive->driveToPoint(4, 0);
+
+  EXPECT_DOUBLE_EQ(distanceController->getTarget(), 0);
+  EXPECT_DOUBLE_EQ(angleController->getTarget(), 0);
+  EXPECT_DOUBLE_EQ(turnController->getTarget(), 0);
+}
+
+TEST_F(OdomChassisControllerPIDTest, MoveAboveThreshold) {
+  assertMotorsHaveBeenStopped(leftMotor, rightMotor);
+
+  drive->setMoveThreshold(5);
+  drive->driveToPoint(6, 0);
+
+  EXPECT_DOUBLE_EQ(distanceController->getTarget(), 6);
+  EXPECT_DOUBLE_EQ(angleController->getTarget(), 0);
+  EXPECT_DOUBLE_EQ(turnController->getTarget(), 0);
+}
+
+TEST_F(OdomChassisControllerPIDTest, SetStateTest) {
   auto stateBefore = drive->getState();
   assertOdomStateEquals(0, 0, 0, stateBefore);
 
