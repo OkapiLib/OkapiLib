3c5b08eb3d2ae4322f510d33cd6792c5570de143
Ryan Benasutti <firey45@gmail.com>
2018-08-16 09:24:57 -0400

    Add copies of the tests for AsyncVelPIDController

--- test/asyncWrapperTests.cpp
+++ test/asyncWrapperTests.cpp
@@ -18,34 +18,56 @@ class AsyncWrapperTest : public ::testing::Test {
     input = std::make_shared<MockContinuousRotarySensor>();
     output = std::make_shared<MockMotor>();
     posPIDController = new AsyncPosPIDController(input, output, createTimeUtil(), 0.1, 0, 0);
+    velPIDController = new AsyncVelPIDController(
+      input,
+      output,
+      createTimeUtil(),
+      0.1,
+      0,
+      0,
+      0,
+      std::make_unique<VelMath>(
+        imev5TPR, std::make_shared<PassthroughFilter>(), std::make_unique<MockTimer>()));
   }
 
   void TearDown() override {
     delete posPIDController;
+    delete velPIDController;
   }
 
   std::shared_ptr<MockContinuousRotarySensor> input;
   std::shared_ptr<MockMotor> output;
   AsyncPosPIDController *posPIDController;
+  AsyncVelPIDController *velPIDController;
 };
 
-TEST_F(AsyncWrapperTest, PosPIDController) {
-}
-
-TEST_F(AsyncWrapperTest, GetTargetTest) {
+TEST_F(AsyncWrapperTest, GetTargetTestPosPID) {
   posPIDController->setTarget(10);
   EXPECT_EQ(posPIDController->getTarget(), 10);
 }
 
-TEST_F(AsyncWrapperTest, SettledWhenDisabled) {
+TEST_F(AsyncWrapperTest, GetTargetTestVelPID) {
+  velPIDController->setTarget(10);
+  EXPECT_EQ(velPIDController->getTarget(), 10);
+}
+
+TEST_F(AsyncWrapperTest, SettledWhenDisabledPosPID) {
   assertControllerIsSettledWhenDisabled(*posPIDController, 100.0);
 }
 
-TEST_F(AsyncWrapperTest, WaitUntilSettledWorksWhenDisabled) {
+TEST_F(AsyncWrapperTest, SettledWhenDisabledVelPID) {
+  assertControllerIsSettledWhenDisabled(*velPIDController, 100.0);
+}
+
+TEST_F(AsyncWrapperTest, WaitUntilSettledWorksWhenDisabledPosPID) {
   assertWaitUntilSettledWorksWhenDisabled(*posPIDController);
 }
 
-TEST_F(AsyncWrapperTest, FollowsDisableLifecycle) {
+TEST_F(AsyncWrapperTest, WaitUntilSettledWorksWhenDisabledVelPID) {
+  assertWaitUntilSettledWorksWhenDisabled(*velPIDController);
+}
+
+TEST_F(AsyncWrapperTest, FollowsDisableLifecyclePosPID) {
   assertControllerFollowsDisableLifecycle(
     *posPIDController,
     output->lastPosition,
@@ -53,8 +75,22 @@ TEST_F(AsyncWrapperTest, FollowsDisableLifecycle) {
     0); // Expected output is 0 since this controller requires stepping in another loop
 }
 
-TEST_F(AsyncWrapperTest, FollowsTargetLifecycle) {
+TEST_F(AsyncWrapperTest, FollowsDisableLifecycleVelPID) {
+  assertControllerFollowsDisableLifecycle(
+    *velPIDController,
+    output->lastPosition,
+    output->lastVoltage,
+    0); // Expected output is 0 since this controller requires stepping in another loop
+}
+
+TEST_F(AsyncWrapperTest, FollowsTargetLifecyclePosPID) {
   assertControllerFollowsTargetLifecycle(
     *posPIDController,
+    0); // Expected output is 0 since this controller requires stepping in another loop
+}
+
+TEST_F(AsyncWrapperTest, FollowsTargetLifecycleVelPID) {
+  assertControllerFollowsTargetLifecycle(
+    *velPIDController,
     0); // Expected output is 0 since this controller requires stepping in another loop
 }
