10cd3287647101e70d82e16cbbc6b481ece6b121
Ryan Benasutti <firey45@gmail.com>
2018-08-21 10:06:23 -0400

    Issue #177: Implement digitalToProsEnum

--- src/impl/device/controllerUtil.cpp
+++ src/impl/device/controllerUtil.cpp
@@ -31,6 +31,31 @@ controller_analog_e_t ControllerUtil::analogToProsEnum(ControllerAnalog in) {
 }
 
 controller_digital_e_t ControllerUtil::digitalToProsEnum(ControllerDigital in) {
-  // TODO: Implement this
+  switch (in) {
+  case ControllerDigital::L1:
+    return E_CONTROLLER_DIGITAL_L1;
+  case ControllerDigital::L2:
+    return E_CONTROLLER_DIGITAL_L2;
+  case ControllerDigital::R1:
+    return E_CONTROLLER_DIGITAL_R1;
+  case ControllerDigital::R2:
+    return E_CONTROLLER_DIGITAL_R2;
+  case ControllerDigital::up:
+    return E_CONTROLLER_DIGITAL_UP;
+  case ControllerDigital::down:
+    return E_CONTROLLER_DIGITAL_DOWN;
+  case ControllerDigital::left:
+    return E_CONTROLLER_DIGITAL_LEFT;
+  case ControllerDigital::right:
+    return E_CONTROLLER_DIGITAL_RIGHT;
+  case ControllerDigital::X:
+    return E_CONTROLLER_DIGITAL_X;
+  case ControllerDigital::B:
+    return E_CONTROLLER_DIGITAL_B;
+  case ControllerDigital::Y:
+    return E_CONTROLLER_DIGITAL_Y;
+  case ControllerDigital::A:
+    return E_CONTROLLER_DIGITAL_A;
+  }
 }
 } // namespace okapi
