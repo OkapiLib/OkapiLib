bb69dc03df8be4fa52db6768237bfc6ac4b79f0e
Ryan Benasutti <firey45@gmail.com>
2018-11-16 16:50:48 -0500

    #279: Add testSensorsWork

--- src/test/chassisControllerBuilderIntegrationTests.cpp
+++ src/test/chassisControllerBuilderIntegrationTests.cpp
@@ -53,8 +53,50 @@ void testMaxVelWorksOutOfOrder() {
   pros::delay(500);
 }
 
+void testSensorsWork() {
+  printf("Testing sensor configuration works\n");
+
+  const double power = 0.2;
+  auto drive = ChassisControllerBuilder()
+                 .withMotors(MOTOR_1_PORT, MOTOR_2_PORT)
+                 .withGearset(MOTOR_GEARSET)
+                 .withSensors({MOTOR_1_PORT}, {MOTOR_2_PORT})
+                 .build();
+
+  Motor leftMtr(MOTOR_1_PORT);
+  Motor rightMtr(MOTOR_2_PORT);
+
+  auto sensorValsBefore = drive->getSensorVals();
+  auto leftBefore = leftMtr.getPosition();
+  auto rightBefore = rightMtr.getPosition();
+
+  drive->forward(power);
+  pros::delay(250);
+
+  drive->stop();
+  auto sensorValsAfter = drive->getSensorVals();
+  auto sensorDiff = sensorValsAfter - sensorValsBefore;
+  auto leftAfter = leftMtr.getPosition();
+  auto rightAfter = rightMtr.getPosition();
+  auto leftDiff = leftAfter - leftBefore;
+  auto rightDiff = rightAfter - rightBefore;
+
+  test("Left sensor diff should be positive",
+       TEST_BODY(AssertThat, sensorDiff[0], IsGreaterThan(0)));
+  test("Left sensor diff should be equal to the diff from the motor",
+       TEST_BODY(AssertThat, sensorDiff[0], EqualsWithDelta(leftDiff, 5)));
+  test("Right sensor diff should be positive",
+       TEST_BODY(AssertThat, sensorDiff[1], IsGreaterThan(0)));
+  test("Right sensor diff should be equal to the diff from the motor",
+       TEST_BODY(AssertThat, sensorDiff[1], EqualsWithDelta(rightDiff, 5)));
+
+  drive->stop();
+  pros::delay(500);
+}
+
 void runChassisControllerFactoryIntegrationTests() {
   test_printf("Testing ChassisControllerBuilder");
   testForwardUsesCorrectMaximumVelocityForAGearset();
   testMaxVelWorksOutOfOrder();
+  testSensorsWork();
 }
