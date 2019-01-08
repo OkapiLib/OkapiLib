94418431e57cbd7e575b3a3d667baffeaa34ad4e
Ryan Benasutti <firey45@gmail.com>
2019-01-08 11:09:47 -0500

    Restore opcontrol

--- src/opcontrol.cpp
+++ src/opcontrol.cpp
@@ -2,16 +2,7 @@
 #include "test/tests/impl/allImplTests.hpp"
 
 using namespace okapi;
-
-void fooFunc(void *) {
-  auto drive = ChassisControllerBuilder().withMotors(1, 1).withGains({}, {}).build();
-  pros::delay(1000);
-}
-
 void opcontrol() {
   pros::delay(100);
-  CrossplatformThread fooTask(fooFunc, nullptr);
-  pros::delay(300);
-  pros::c::task_delete(fooTask.thread);
-  pros::delay(10000);
+  runAllImplTests();
 }
