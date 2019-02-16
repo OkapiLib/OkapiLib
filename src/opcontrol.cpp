b79944fd7bd632e17e57c94dab9191d683ffb617
Ryan Benasutti <firey45@gmail.com>
2019-02-15 22:51:45 -0500

    #249: Restore opcontrol

--- src/opcontrol.cpp
+++ src/opcontrol.cpp
@@ -1,17 +1,8 @@
 #include "main.h"
-#include "pros/apix.h"
 #include "test/tests/impl/allImplTests.hpp"
 
 using namespace okapi;
-
-void task1(void *) {
-  auto drive = ChassisControllerBuilder().withMotors(-18, 19).withGains({}, {}).build();
-  pros::delay(100);
-}
-
 void opcontrol() {
   pros::delay(100);
-  pros::Task t1(task1);
-
-  pros::delay(50);
+  runAllImplTests();
 }
