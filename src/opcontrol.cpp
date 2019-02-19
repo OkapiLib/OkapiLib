b40db58873fbf9764c806faa40569b28d53cb35c
Ryan Benasutti <firey45@gmail.com>
2019-02-19 11:52:23 -0500

    Restore tests in opcontrol

--- src/opcontrol.cpp
+++ src/opcontrol.cpp
@@ -3,9 +3,6 @@
 
 using namespace okapi;
 void opcontrol() {
-  auto drive = ChassisControllerBuilder()
-                 .withMotors(1, -2) // Left motor is 1, right motor is 2 (reversed)
-                 .withLogger(std::make_shared<Logger>(
-                   TimeUtilFactory::create().getTimer(), "/ser/sout", Logger::LogLevel::debug))
-                 .build();
+  pros::delay(100);
+  runAllImplTests();
 }
