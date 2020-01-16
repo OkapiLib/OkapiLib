f025ccaafd4da81319759631e254b55dde096afa
Octogonapus <firey45@gmail.com>
2020-01-15 22:49:24 -0500

    Update chassisScales.hpp

--- include/okapi/api/chassis/controller/chassisScales.hpp
+++ include/okapi/api/chassis/controller/chassisScales.hpp
@@ -28,7 +28,7 @@ class ChassisScales {
    * meaning the width between the centers of both wheels). For example, if you are using four inch
    * omni wheels and there are 11.5 inches between the centers of each wheel, you would call the
    * constructor like so:
-   *   ChassisScales scales({4_in, 11.5_in});
+   *   ChassisScales scales({4_in, 11.5_in}, imev5GreenTPR); // imev5GreenTPR for a green gearset
    *
    *                             Wheel diameter
    *
