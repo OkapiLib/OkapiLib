fcde2d86b7145726d8a903ed9b8e8369d6fedc86
Wesley Chalmers <wesleyjchalmers@gmail.com>
2020-07-06 22:30:03 -0400

    #302: Add unit tests for RQuantity math functions

--- test/unitTests.cpp
+++ test/unitTests.cpp
@@ -8,6 +8,7 @@
 #include "okapi/api/units/QArea.hpp"
 #include "okapi/api/units/QLength.hpp"
 #include "okapi/api/units/QTime.hpp"
+#include "okapi/api/units/QVolume.hpp"
 #include <gtest/gtest.h>
 
 using namespace okapi;
@@ -43,4 +44,80 @@ TEST(UnitTests, UnaryMinusTest) {
 
 TEST(UnitTests, SqrtTest) {
   EXPECT_DOUBLE_EQ(std::sqrt(2), (2 * meter2).sqrt().convert(meter));
+}
+
+TEST(UnitTests, AbsFreeFunction) {
+  EXPECT_EQ(abs(3.0_m), 3.0_m);
+  EXPECT_EQ(abs(-3.0_m), 3.0_m);
+}
+
+TEST(UnitTests, PowRootTest) {
+  EXPECT_EQ(pow<2>(3_in), 3_in * 3_in);
+  EXPECT_EQ(root<3>(3_in * 3_in * 3_in), 3_in);
+  EXPECT_EQ(pow<std::ratio<3, 2>>(3_in), pow<3>(root<2>(3_in)));
+}
+
+TEST(UnitTests, SquareCubeTest) {
+  EXPECT_EQ(square(QLength(3)), QArea(9));
+  EXPECT_EQ(cube(QLength(4)), QVolume(64));
+  EXPECT_EQ(sqrt(QArea(25)), QLength(5));
+  EXPECT_EQ(cbrt(QVolume(8)), QLength(2));
+  EXPECT_EQ(hypot(QLength(3), QLength(4)), QLength(5));
+}
+
+TEST(UnitTests, ModTest) {
+  EXPECT_EQ(mod(QLength(4), QLength(2)), QLength(0));
+  EXPECT_EQ(mod(10_in, 3_in), 1_in);
+}
+
+TEST(UnitTests, CopysignTest) {
+  EXPECT_EQ(copysign(3_m, -4_in), -3_m);
+  EXPECT_EQ(copysign(1_s, 5_m), 1_s);
+}
+
+TEST(UnitTests, RoundTest) {
+  EXPECT_EQ(ceil(4.1_in, inch), 5_in);
+  EXPECT_EQ(ceil(-4.1_in, inch), -4_in);
+
+  EXPECT_EQ(floor(1.17_s, 100_ms), 1.1_s);
+  EXPECT_EQ(floor(-1.17_s, 100_ms), -1.2_s);
+
+  EXPECT_EQ(trunc(5.8_cm, centimeter), 5_cm);
+  EXPECT_EQ(trunc(-5.8_cm, centimeter), -5_cm);
+
+  EXPECT_EQ(round(2.4_in, inch), 2_in);
+  EXPECT_EQ(round(2.6_in, inch), 3_in);
+  EXPECT_EQ(round(-2.4_in, inch), -2_in);
+  EXPECT_EQ(round(-2.6_in, inch), -3_in);
+}
+
+TEST(UnitTests, TrigTest) {
+  EXPECT_EQ(sin(30_deg), Number(0.5));
+  EXPECT_EQ(cos(60_deg), Number(0.5));
+  EXPECT_EQ(tan(45_deg), Number(1));
+}
+
+TEST(UnitTests, InverseTrigTest) {
+  EXPECT_EQ(asin(Number(0.5)), 30_deg);
+  EXPECT_EQ(acos(Number(0.5)), 60_deg);
+  EXPECT_EQ(atan(Number(1)), 45_deg);
+}
+
+TEST(UnitTests, HyperbTest) {
+  EXPECT_DOUBLE_EQ(sinh(37_deg).convert(radian), 0.6916004653045855);
+  EXPECT_DOUBLE_EQ(cosh(37_deg).convert(radian), 1.2158582169025791);
+  EXPECT_DOUBLE_EQ(tanh(37_deg).convert(radian), 0.5688167055090109);
+}
+
+TEST(UnitTests, InverseHyperbTest) {
+  EXPECT_DOUBLE_EQ(asinh(Number(0.6916004653045855)).convert(degree), 37.0);
+  EXPECT_DOUBLE_EQ(acosh(Number(1.2158582169025791)).convert(degree), 37.0);
+  EXPECT_DOUBLE_EQ(atanh(Number(0.5688167055090109)).convert(degree), 37.0);
+}
+
+TEST(UnitTests, Atan2Test) {
+  EXPECT_DOUBLE_EQ(atan2(1_ft, 2_ft).convert(radian), 0.4636476090008061);
+  EXPECT_DOUBLE_EQ(atan2(1_ft, -2_ft).convert(radian), 2.677945044588987);
+  EXPECT_DOUBLE_EQ(atan2(-1_ft, -2_ft).convert(radian), -2.677945044588987);
+  EXPECT_DOUBLE_EQ(atan2(-1_ft, 2_ft).convert(radian), -0.4636476090008061);
 }
