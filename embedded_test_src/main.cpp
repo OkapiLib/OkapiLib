24df970abdfacb5b5beecf9ed433bf5fa94de4cb
Octogonapus <firey45@gmail.com>
2022-08-07 14:57:00 -0400

    Move embedded_test_src out of src and remove duplicate main definition

--- embedded_test_src/main.cpp
+++ embedded_test_src/main.cpp
@@ -3,30 +3,42 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/.
  */
-#include "pros/rtos.hpp"
+#include "main.h"
 #include "unity/unity.h"
 
 void setUp(void) {
-    // set stuff up here
+  // set stuff up here
 }
 
 void tearDown(void) {
-    // clean stuff up here
+  // clean stuff up here
 }
 
 void test_function_should_doBlahAndBlah(void) {
-    //test stuff
+  // test stuff
 }
 
 void test_function_should_doAlsoDoBlah(void) {
-    //more test stuff
+  // more test stuff
 }
 
 // not needed when using generate_test_runner.rb
-int main(void) {
-    pros::delay(1000);
-    UNITY_BEGIN();
-    RUN_TEST(test_function_should_doBlahAndBlah);
-    RUN_TEST(test_function_should_doAlsoDoBlah);
-    return UNITY_END();
+void opcontrol() {
+  pros::delay(1000);
+  UNITY_BEGIN();
+  RUN_TEST(test_function_should_doBlahAndBlah);
+  RUN_TEST(test_function_should_doAlsoDoBlah);
+  UNITY_END();
+}
+
+void initialize() {
+}
+
+void disabled() {
+}
+
+void competition_initialize() {
+}
+
+void autonomous() {
 }
